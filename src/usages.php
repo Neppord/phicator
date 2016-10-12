b97201ba43e96a24291e8336db1a27b59861a4ed
Samuel Ytterbrink <neppord@gmail.com>
2016-10-12 23:18:17 +0200

    Simple and not fully functional usage script built.

---
+++ src/usages.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace Phocate;
+
+use PDO;
+
+ini_set('memory_limit', '1024M');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+$in = $argv[1];
+$like = '%';
+for($i = 0; isset($in[$i]); $i += 1) {
+    $like .= $in[$i] . '%';
+}
+$pdo = new PDO('sqlite:phocate.db');
+$stmt = $pdo->prepare(
+    "SELECT class_path, usage_path, FQN FROM usages JOIN classes USING (FQN) WHERE FQN LIKE ? "
+);
+$stmt->execute([$like]);
+$results = $stmt->fetchAll();
+if (empty($results)) {
+    echo "No usages found\n";
+} else {
+    foreach($results as $result) {
+        $FQN = $result['FQN'];
+        $class_path = $result['class_path'];
+        $usage_path = $result['usage_path'];
+        echo "class $FQN in file $class_path is used in $usage_path\n";
+    }
+}
