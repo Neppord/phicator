1b9272c371e085fc2eff946fe7cefe2f012a97ca
Samuel Ytterbrink <neppord@gmail.com>
2016-10-13 13:02:28 +0200

    Add extends to database.

--- src/phocate.php
+++ src/phocate.php
@@ -17,6 +17,7 @@ $sql = "BEGIN;\n";
 $sql .= "CREATE TABLE IF NOT EXISTS namespaces (namespace_path TEXT, namespace TEXT);\n";
 $sql .= "CREATE TABLE IF NOT EXISTS usages (usage_path TEXT, namespace TEXT, FQN TEXT, name TEXT);\n";
 $sql .= "CREATE TABLE IF NOT EXISTS classes (class_path TEXT, namespace TEXT, FQN TEXT, name TEXT);\n";
+$sql .= "CREATE TABLE IF NOT EXISTS extends (FQN TEXT, super_FQN TEXT);\n";
 foreach($project_dir->getPhpFiles() as $php_file) {
     $path = $php_file->getPath();
     $tokens = $php_file->getTokens();
@@ -32,7 +33,22 @@ foreach($project_dir->getPhpFiles() as $php_file) {
         }
         foreach ($namespace->classes as $class) {
             $name = $class->name;
-            $sql .= "INSERT OR REPLACE INTO classes (class_path, namespace, FQN, name) VALUES (\"$path\", \"$namespace_name\", \"$namespace_name\\$name\", \"$name\");\n";
+            $FQN = "$namespace_name\\$name";
+            $sql .= "INSERT OR REPLACE INTO classes (class_path, namespace, FQN, name) VALUES (\"$path\", \"$namespace_name\", \"$FQN\", \"$name\");\n";
+            if ($class->extends != '') {
+                $extends = $class->extends;
+                if ($extends[0] === '\\') {
+                    $super_FQN = "$extends";
+                } else {
+                    $super_FQN = "$namespace_name\\$extends";
+                    foreach($namespace->usages as $usage) {
+                        if ($usage->name === $extends) {
+                            $super_FQN = $usage->FQN;
+                        }
+                    }
+                }
+                $sql .= "INSERT OR REPLACE INTO extends VALUES (\"$FQN\", \"$super_FQN\");\n";
+            }
         }
     }
 }
