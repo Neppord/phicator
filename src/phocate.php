b1d18b42f43b4ffb9b5ed1e2b7fb4b6240b66676
Samuel Ytterbrink <neppord@gmail.com>
2017-04-17 19:25:48 +0200

    Fix sql bug and move sql to prepared.

--- src/phocate.php
+++ src/phocate.php
@@ -16,8 +16,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $file_p = new FileParser();
 
 $project_dir = new Directory($argv[1]);
-$sql = "BEGIN;\n";
-$sql .= file_get_contents(__DIR__ . '/schema.sql');
+$pdo = new PDO('sqlite:phocate.db');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->exec('BEGIN');
+$schema = file_get_contents(__DIR__ . '/schema.sql');
+$pdo->exec($schema);
+$insert_namespace = $pdo->prepare("INSERT OR REPLACE INTO namespaces (namespace_path, namespace) VALUES (:namespace_path, :namespace)");
+$insert_class = $pdo->prepare("INSERT OR REPLACE INTO classes (class_path, namespace, FQN, name) VALUES (:class_path, :namespace, :FQN, :name)");
+$insert_use = $pdo->prepare("INSERT OR REPLACE INTO usages (usage_path, namespace, FQN, name) VALUES (:usage_path, :namespace, :FQN, :name);");
+$insert_extends = $pdo->prepare("INSERT OR REPLACE INTO extends (FQN, super_FQN) VALUES (:FQN, :super_FQN)");
+$insert_implements = $pdo->prepare("INSERT OR REPLACE INTO implements (FQN, interface_FQN) VALUES (:FQN, :interface_FQN);");
 foreach($project_dir->getPhpFiles() as $php_file) {
     $path = $php_file->getPath();
     $tokens = $php_file->getTokens();
@@ -25,16 +33,29 @@ foreach($project_dir->getPhpFiles() as $php_file) {
     $result = $file_p->parser($path, $tokens);
     foreach ($result->file->namespaces as $namespace) {
         $namespace_name = $namespace->name;
-        $sql .= "INSERT OR REPLACE INTO namespaces (namespace_path, namespace) VALUES (TEXT \"$path\", TEXT \"$namespace_name\");\n";
+        $insert_namespace->execute([
+            ':namespace_path' => $path,
+            ':namespace' => $namespace_name
+        ]);
         foreach ($namespace->usages as $use) {
             $name = $use->name;
             $FQN = $use->FQN;
-            $sql .= "INSERT OR REPLACE INTO usages (usage_path, namespace, FQN, name) VALUES (TEXT \"$path\",TEXT \"$namespace_name\", TEXT \"$FQN\", TEXT \"$name\");\n";
+            $insert_use->execute([
+                ':namespace' => $namespace_name,
+                ':usage_path' => $path,
+                ':name' => $name,
+                ':FQN' => $FQN
+            ]);
         }
         foreach ($namespace->classes as $class) {
             $name = $class->name;
             $FQN = "$namespace_name\\$name";
-            $sql .= "INSERT OR REPLACE INTO classes (class_path, namespace, FQN, name) VALUES (TEXT \"$path\", TEXT \"$namespace_name\", TEXT \"$FQN\", TEXT \"$name\");\n";
+            $insert_class->execute([
+                ':class_path' => $path,
+                ':namespace' => $namespace_name,
+                ':FQN' => $FQN,
+                ':name' => $name
+            ]);
             if ($class->extends != '') {
                 $extends = $class->extends;
                 if ($extends[0] === '\\') {
@@ -47,7 +68,10 @@ foreach($project_dir->getPhpFiles() as $php_file) {
                         $super_FQN = "$namespace_name\\$extends";
                     }
                 }
-                $sql .= "INSERT OR REPLACE INTO extends VALUES (TEXT \"$FQN\", TEXT \"$super_FQN\");\n";
+                $insert_extends->execute([
+                    ':FQN' => $FQN,
+                    ':super_FQN' => $super_FQN
+                ]);
             }
             foreach ($class->implements as $interface) {
                 if ($interface[0] === '\\') {
@@ -60,12 +84,12 @@ foreach($project_dir->getPhpFiles() as $php_file) {
                         $interface_FQN = "$namespace_name\\$interface";
                     }
                 }
-                $sql .= "INSERT OR REPLACE INTO implements VALUES (TEXT \"$FQN\", TEXT \"$interface_FQN\");\n";
+                $insert_implements->execute([
+                    ':FQN' => $FQN,
+                    ':interface_FQN' => $interface_FQN
+                ]);
             }
         }
     }
 }
-$sql .= 'END;';
-
-$pdo = new PDO('sqlite:phocate.db');
-$pdo->exec($sql);
+$pdo->exec('END;');
